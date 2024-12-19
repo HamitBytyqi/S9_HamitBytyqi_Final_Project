45aac2eeca177ffcdfb0dafc013755e68864747c
HamitBytyqi <hb51415@ubt-uni.net>
2024-12-19 23:32:59 +0100

    Full Project

---
+++ js/slider.js
@@ -0,0 +1,21 @@
+var sliderArray = ['url(images/slider1.png)', 'url(images/slider2.png)', 'url(images/slider3.png)'];
+
+var i = 0;
+
+function slideShow() {
+    document.getElementById('slides-pics').style.backgroundImage = sliderArray[i];
+    if (i < sliderArray.length - 1) {
+        i++;
+        document.getElementById('slides-pics').style.transition = "0.5s ease-in-out";
+    }
+    else {
+        i = 0;
+    }
+
+
+    setTimeout('slideShow()', 2000);
+
+}
+
+
+slideShow();
